e55df6ac449d517aa7c609991c90c00f97357491
pgonzalesc <pedro281195@gmail.com>
2020-10-24 10:03:58 -0500

    Modificar ejercicio 6

--- Ejercicio6.js
+++ Ejercicio6.js
@@ -1,19 +1,49 @@
 /*6. Crea una función usando callbacks, que reciba una condición, y dos
 funciones que se realicen en caso la condición se cumpla o no.*/
 
-const printPar = ()=> console.log('Es par');
-const printImpar = ()=> console.log('Es impar');
+const orderAsc = (array)=> {
+    let aux;
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[j] > array[j+1]){
+                aux = array[j];
+                array[j] = array[j+1];
+                array[j+1] = aux;
+            }
+        }
+    }
+    return array;
+};
 
-const evaluate = (num)=> {
-    if(num%2 == 0)
-        printPar();
-    else
-        printImpar();
+const orderDesc = (array)=> {
+    let aux;
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[j] < array[j+1]){
+                aux = array[j];
+                array[j] = array[j+1];
+                array[j+1] = aux;
+            }
+        }
+    }
+    return array;
+};
+
+const order = (array, orderBy='ASC')=> {
+    if(orderBy == 'ASC')
+        return orderAsc(array);
+    else if(orderBy == 'DESC')
+        return orderDesc(array);
 }
 
-const myCallback = (number, callback)=> {
-    callback(number);
+const myCallback = (array, callback, orderBy)=> {
+    return callback(array, orderBy);
 }
+let fruits = ['Manzana', 'Pera', 'Mandarina', 'Piña', 'Fresa'];
+let numbers = [50, 5, 10, 32, 3];
 
-myCallback(5,evaluate);
-myCallback(6,evaluate);
+console.log(myCallback(fruits, order, 'ASC'));
+console.log(myCallback(fruits, order, 'DESC'));
+
+console.log(myCallback(numbers, order, 'ASC'));
+console.log(myCallback(numbers, order, 'DESC'));
